8b69086a863508bf125d4d0766ed3e4a3765db89
Satanshu Mishra <satanshumishra@outlook.com>
2024-04-11 01:01:04 -0700

    Auto-commit: Update Neovim config

--- sync_nvim.sh
+++ sync_nvim.sh
@@ -3,9 +3,9 @@
 cd .config
 git add .
 git commit -m "Syncing Nvim config changes"
-git push origin master  # Assuming 'master' is your config repo's main branch
+git push origin main  # Assuming 'master' is your config repo's main branch
 
 cd ..
 git add .config
 git commit -m "Updated Nvim config"
-git push origin master  # Assuming 'master' is your main repo's main branch
+git push origin main  # Assuming 'master' is your main repo's main branch
